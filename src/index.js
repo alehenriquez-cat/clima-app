01b5250b8166e4852b3c69002a67e2e8656e358f
alehenriquez-cat <alejandranhenriqueza@gmail.com>
2025-05-03 16:28:53 +1000

    updated search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#app-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "a488e2130c3et2d5ao1ef3ffc71e4b14";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Melbourne");
